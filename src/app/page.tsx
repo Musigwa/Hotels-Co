785971eec72ea16ffb24e22b2c29d26cd3e9b20c
Musigwa Pacifique <68857067+Musigwa@users.noreply.github.com>
2023-10-27 01:51:03 +0200

    Refactor the infinite scrool side effect for more code readability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,21 +33,14 @@ export default function Home() {
     });
 
   useEffect(() => {
-    const handleScroll = () => {
+    window.onscroll = () => {
       const isBottom =
         window.innerHeight + document.documentElement.scrollTop >=
         document.documentElement.scrollHeight - 1;
-      if (isBottom && hasNextPage && !isFetching) {
-        console.log('isBottom', isBottom);
-        if (isBottom && hasNextPage && !isFetchingNextPage) {
-          fetchNextPage();
-        }
-      }
+      if (isBottom && hasNextPage && !isFetching && !isFetchingNextPage) fetchNextPage();
     };
-    window.addEventListener('scroll', handleScroll);
-
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.onscroll = null;
     };
   }, [hasNextPage, isFetchingNextPage, fetchNextPage, isFetching]);
 
@@ -55,34 +48,36 @@ export default function Home() {
   if (error) return 'An error has occurred: ' + error.message;
   if (!data || !data.pages[0]) return 'No data matching your search!';
   return (
-    <div className='mb-20 grid text-center lg:max-w-7xl lg:w-full lg:mb-0 lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 lg:text-left gap-10'>
-      {data?.pages.map((page: Room[], idx: number) =>
-        page.map((room: Room, id: number) => (
-          <Link href={{ pathname: `/rooms`, query: room }} key={id} className='group'>
-            <div className='aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 dark:bg-white-800/30 xl:aspect-h-8 xl:aspect-w-7'>
-              <Image
-                src={room.thumbnail}
-                alt={room.imageAlt}
-                width={0}
-                height={0}
-                className='h-full w-full object-cover object-center group-hover:opacity-75'
-                layout='responsive'
-              />
-            </div>
-            <h3 className='mt-4 text-sm text-gray-700 dark:text-gray-300'>{room.name}</h3>
-            <p className='mt-1 text-lg font-medium text-gray-900 dark:text-gray-400'>
-              ${room.price}
-            </p>
-          </Link>
-        ))
-      )}
-      <div>
+    <>
+      <div className='mb-20 grid text-center lg:max-w-7xl lg:w-full lg:mb-0 lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 lg:text-left gap-10'>
+        {data?.pages.map((page: Room[], idx: number) =>
+          page.map((room: Room, id: number) => (
+            <Link href={{ pathname: `/rooms`, query: room }} key={id} className='group'>
+              <div className='aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 dark:bg-white-800/30 xl:aspect-h-8 xl:aspect-w-7'>
+                <Image
+                  src={room.thumbnail}
+                  alt={room.imageAlt}
+                  width={0}
+                  height={0}
+                  className='h-full w-full object-cover object-center group-hover:opacity-75'
+                  layout='responsive'
+                />
+              </div>
+              <h3 className='mt-4 text-sm text-gray-700 dark:text-gray-300'>{room.name}</h3>
+              <p className='mt-1 text-lg font-medium text-gray-900 dark:text-gray-400'>
+                ${room.price}
+              </p>
+            </Link>
+          ))
+        )}
+      </div>
+      <div className='w-full text-center m-10'>
         {isFetchingNextPage
           ? 'Loading more...'
           : hasNextPage
-          ? 'Load More'
-          : 'Nothing more to load'}
+          ? 'Click to load More'
+          : 'No more rooms to show...'}
       </div>
-    </div>
+    </>
   );
 }
